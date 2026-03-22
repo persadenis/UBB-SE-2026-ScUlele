c64465d8846dd2a04446630fd405c88e216dcbfc
Rari1919 <rarespetrule0@gmail.com>
2026-03-22 21:12:41 +0200

    C-02 Add Team C configuration support

--- BankApp/BankApp.Server/Configuration/TeamCOptions.cs
+++ BankApp/BankApp.Server/Configuration/TeamCOptions.cs
@@ -3,6 +3,7 @@ namespace BankApp.Server.Configuration
     public class TeamCOptions
     {
         public const string SectionName = "TeamC";
+
         public int CardRevealDurationSeconds { get; set; } = 30;
         public decimal MaximumSpendingLimit { get; set; } = 100000m;
         public int BalanceTrendDays { get; set; } = 30;
